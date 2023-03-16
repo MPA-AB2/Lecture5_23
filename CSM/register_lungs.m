458603eee7705433024d5df31210de1311c505ff
jakubmull <91261182+jakubmull@users.noreply.github.com>
2023-03-16 17:31:42 +0100

    Add files via upload

--- CSM/register_lungs.m
+++ CSM/register_lungs.m
@@ -2,36 +2,37 @@ function register_lungs(pathMain)
 
 for i = 1:3
 path = [pathMain,'\pat0',num2str(i),'\'];
-
+fixed = niftiread([path,'fixed.nii']);
 moving = niftiread([path,'moving.nii']);
 
-mask = zeros(size(moving));
-mask(moving<12000) = 1;
-mask(moving>12000) = 0;
+mMask = zeros(size(moving));
+mMask(moving<9500) = 1;
+mMask(moving>9500) = 0;
 
-niftiwrite(mask,"mask.nii")
-movefile ("mask.nii", path)
+fMask = zeros(size(fixed));
+fMask(fixed<9500) = 1;
+fMask(fixed>9500) = 0;
+
+niftiwrite(mMask,"mMask.nii")
+movefile ("mMask.nii", path)
+
+niftiwrite(fMask,"fMask.nii")
+movefile ("fMask.nii", path)
+
 mkdir TempFile
-
 fixedImg = ([path,'fixed.nii']);
 movingImg = ([path,'moving.nii']);
-maskImg = ([path,'mask.nii']);
+mMaskImg = ([path,'mMask.nii']);
+fMaskImg = ([path,'fMask.nii']);
 outputDirectory = ('TempFile');
-addpath('parameter_files');
-paramPath= which('Parameters_Rigid.txt');
+paramPath= which('Parameters_Affine.txt');
 parameterFile = (paramPath);
 
-cmd = append('elastix\elastix.exe', ' -f ', fixedImg, ' -m ', movingImg,' -mMask ', maskImg, ' -out ', outputDirectory, ' -p ', parameterFile);
+cmd = append('elastix\elastix.exe', ' -f ', fixedImg, ' -fMask ', fMaskImg,' -m ', movingImg,' -mMask ', mMaskImg, ' -out ', outputDirectory, ' -p ', parameterFile);
 % run ELASTIX
 system(cmd);
-
-movefile ('TempFile\TransformParameters.0.txt',path)
-
 outputDirectory = ("TempFile");
-
-
-% which
-inputFile = ([path,'TransformParameters.0.txt']);
+inputFile = ('TempFile\TransformParameters.0.txt');
 cmdT = append('elastix\transformix.exe', ' -def all -out ', outputDirectory, ' -tp ', inputFile);
 
 % run TRANSFORMIX
@@ -39,11 +40,7 @@ system(cmdT);
 
 movefile('TempFile\deformationField.nii','TempFile\deformationField_Est.nii')
 movefile('TempFile\deformationField_Est.nii',path)
-
-% Delete Temp directory
-dirpath = 'TempFile';
-delete(fullfile(dirpath, '*'));
-rmdir TempFile
+% Deletes TEMP
+rmdir TempFile s
 end
-
 end
