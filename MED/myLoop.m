10412c95dafb7284a0c419e52b1d138d24295969
dslavicek <211212@vut.cz>
2023-03-16 14:46:09 +0100

    repaired path

--- MED/myLoop.m
+++ MED/myLoop.m
@@ -12,20 +12,20 @@ end
 wholeThing = {};
 for i = 1:size(folders,1)
     %creating masks
-    fMask = niftiread(strcat(mainPath,'Data\',folders{i},'\fixed.nii'));
+    fMask = niftiread(strcat(mainPath,folders{i},'\fixed.nii'));
     fMask = uint8(fMask>0);
-    niftiwrite(fMask, strcat(mainPath,'Data\',folders{i},'\fmask.nii'));
-    mMask = niftiread(strcat(mainPath,'Data\',folders{i},'\moving.nii'));
+    niftiwrite(fMask, strcat(mainPath,folders{i},'\fmask.nii'));
+    mMask = niftiread(strcat(mainPath,folders{i},'\moving.nii'));
     mMask = uint8(mMask>0);
-    niftiwrite(mMask, strcat(mainPath,'Data\',folders{i},'\mmask.nii'))
+    niftiwrite(mMask, strcat(mainPath,folders{i},'\mmask.nii'))
     %paths
-    fixed = strcat(' -f',{' '},mainPath,'Data\',folders{i},'\fixed.nii');
-    moving = strcat(' -m',{' '},mainPath,'Data\',folders{i},'\moving.nii');
+    fixed = strcat(' -f',{' '},mainPath,folders{i},'\fixed.nii');
+    moving = strcat(' -m',{' '},mainPath,folders{i},'\moving.nii');
     outputDir = strcat(' -out',{' '},mainPath,'Temp\');
-    mMask = strcat(' -mMask',{' '},mainPath,'Data\',folders{i},'\mmask.nii');
-    fMask = strcat(' -fMask',{' '},mainPath,'Data\',folders{i},'\fmask.nii');
+    mMask = strcat(' -mMask',{' '},mainPath,folders{i},'\mmask.nii');
+    fMask = strcat(' -fMask',{' '},mainPath,folders{i},'\fmask.nii');
     wholeThing{i,1} = strcat(elastixPathExe,fixed,moving,outputDir,params,mMask,fMask);
-    wholeThing{i,2} = strcat(mainPath,'Data\',folders{i},'\deformationField_Est.nii');
+    wholeThing{i,2} = strcat(mainPath,folders{i},'\deformationField_Est.nii');
 end
 wholeThing{1,3} =strcat(transformixPathExe, ' -def all -out', {' '},mainPath,'Temp -tp', {' '}, mainPath,'Temp\TransformParameters.0.txt');
 wholeThing{1,4} =strcat(mainPath,'Temp\deformationField.nii');
