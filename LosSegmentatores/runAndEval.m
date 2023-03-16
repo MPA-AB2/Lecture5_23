218063cec87582e1cb01c505695a26399649e0ad
ondrejnantl <xnantl01@vutbr.cz>
2023-03-16 14:19:25 +0100

    Signed-off-by: ondrejnantl <xnantl01@vutbr.cz>

--- LosSegmentatores/runAndEval.m
+++ LosSegmentatores/runAndEval.m
@@ -5,12 +5,12 @@ pathData = 'C:\Users\xnantl01\MPA-AB2\Lecture5_23\Data';
 register_lungs(pathData);
 %% evaluate results
 [MSE, meanVar] = eval_lung(pathData);
-%% show 
-fieldGT = niftiread("C:\Users\xnantl01\MPA-AB2\Lecture5_23\Data\pat01\deformationField.nii");
-fieldEst = niftiread("C:\Users\xnantl01\MPA-AB2\Lecture5_23\Data\pat01\deformationField_Est.nii");
-
-figure
-subplot 121
-imshow(fieldGT(:,:,1,1,1),[])
-subplot 122
-imshow(fieldEst(:,:,1,1,1),[])
+%% show deformation fields 
+% fieldGT = niftiread("C:\Users\xnantl01\MPA-AB2\Lecture5_23\Data\pat01\deformationField.nii");
+% fieldEst = niftiread("C:\Users\xnantl01\MPA-AB2\Lecture5_23\Data\pat01\deformationField_Est.nii");
+% 
+% figure
+% subplot 121
+% imshow(fieldGT(:,:,1,1,1),[])
+% subplot 122
+% imshow(fieldEst(:,:,1,1,1),[])
