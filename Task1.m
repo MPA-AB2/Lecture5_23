17fac42fe397c10aa4da81a476937edc15891114
jakubmull <91261182+jakubmull@users.noreply.github.com>
2023-03-16 11:06:22 +0100

    Add files via upload

--- Task1.m
+++ Task1.m
@@ -22,27 +22,37 @@ title('Moving')
 
 
 
-%% Registratio - RIGID AND AFFINE + masked affine
-
+% Registratio - RIGID AND AFFINE + masked affine
 % create TempFile, define the parameters and paths
 
+% Set the directory path
+dirpath = 'TempFile';
+
+% Delete all files in the directory
+delete(fullfile(dirpath, '*'));
+
+fixedImg = ("fixed1.nii");
+movingImg = ("moving1.nii");
 
 
+outputDirectory = ("TempFile");
+parameterFile = ("parameter_files/Parameters_Rigid.txt");
+
+% elastix -f fixedImage.ext -m movingImage.ext -out outputDirectory -p parameterFile.txt
+cmd = append('elastix\elastix.exe', ' -f ', fixedImg, ' -m ', movingImg, ' -out ', outputDirectory, ' -p ', parameterFile);
 
 
 % run ELASTIX
 
+system(cmd);
 
 
 
+%% read resulting nii a save to variable - registered
+registered = niftiread('TempFile/result.0.nii');
+% imshow(result1,[])
 
-% read resulting nii a save to variable - registered
-
-
-
-
-
-%% display
+% display
 subplot(1,3,3)
 imshow(registered,[])
 title('Registered')
@@ -52,3 +62,27 @@ subplot(1,2,1)
 imshowpair(fixed,moving)
 subplot(1,2,2)
 imshowpair(fixed,registered)
+
+
+%% Registratio - RIGID AND AFFINE + masked affine TRANSFORMIX
+% create TempFile, define the parameters and paths
+% clear
+% Set the directory path
+dirpath = 'TempFile';
+
+% Delete all files in the directory
+delete(fullfile(dirpath, '*'));
+
+outputDirectory = ("TempFile");
+parameterFile = ("parameter_files/Parameters_Rigid.txt");
+inputFile = ("parameter_files/TransformParameters.0.txt");
+% movingImg = ("parameter_files/result.0.nii");
+% elastix -f fixedImage.ext -m movingImage.ext -out outputDirectory -p parameterFile.txt
+
+cmd = append('elastix\transformix.exe', ' -def all -out ', outputDirectory, ' -tp ', inputFile);
+% cmd = append('elastix\elastix.exe', ' -f ', fixedImg, ' -m ', movingImg, ' -out ', outputDirectory, ' -p ', parameterFile);
+% run TRANSFORMIX
+system(cmd);
+%
+deformation = niftiread('TempFile/deformationField.nii');
+imshow(deformation(:,:,:,1,1),[])
