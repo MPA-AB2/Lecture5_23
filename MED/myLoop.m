073dd77276ac0bfa97e708a267474d9fc6e14666
Milan Ladicky <xladic00@vutbr.cz>
2023-03-16 12:29:45 +0100

    loop updated

--- MED/myLoop.m
+++ MED/myLoop.m
@@ -1,8 +1,11 @@
 function wholeThing = myLoop(mainPath, elastixPath,paramPath)
-%elastixPath = 'C:\Elastix\elastix-5.1.0-win64\elastix.exe';
-%mainPath =  'V:\Ladicky\AB2\cv5\';
+%elastixPath = 'C:\Elastix\elastix-5.1.0-win64\';
+%mainPath =  'V:\Ladicky\AB2\cv5\Data\';
 folders= {'pat01';'pat02';'pat03'};
 params = [];
+elastixPathExe = [elastixPath,'elastix.exe'];
+transformixPathExe = [elastixPath,'transformix.exe'];
+
 for i = 1:size(paramPath,2)
     params = strcat(params,' -p',{' '},paramPath{i});
 end
@@ -18,10 +21,15 @@ for i = 1:size(folders,1)
     %paths
     fixed = strcat(' -f',{' '},mainPath,'Data\',folders{i},'\fixed.nii');
     moving = strcat(' -m',{' '},mainPath,'Data\',folders{i},'\moving.nii');
-    outputDir = strcat(' -out',{' '},mainPath,'Data\',folders{i});
+    outputDir = strcat(' -out',{' '},mainPath,'Temp\');
     mMask = strcat(' -mMask',{' '},mainPath,'Data\',folders{i},'\mmask.nii');
     fMask = strcat(' -mMask',{' '},mainPath,'Data\',folders{i},'\fmask.nii');
-    wholeThing{i} = strcat(elastixPath,fixed,moving,outputDir,params,mMask,fMask);
+    wholeThing{i,1} = strcat(elastixPathExe,fixed,moving,outputDir,params,mMask,fMask);
+    wholeThing{i,2} = strcat(mainPath,'Data\',folders{i},'\deformationField_Est.nii');
+end
+wholeThing{1,3} =strcat(transformixPathExe, '-def all -out', {' '},mainPath,' -tp', {' '}, mainPath,'Temp\TransformParameters.0.txt');
+wholeThing{1,4} =strcat(mainPath,'Temp\deformationField.nii');
+
 end
 
 
