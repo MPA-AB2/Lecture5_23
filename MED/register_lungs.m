85401cd607d97a8e16f4b6d09bc6b7ea351707a1
dslavicek <211212@vut.cz>
2023-03-16 15:04:43 +0100

    repaired Temp folder generation

--- MED/register_lungs.m
+++ MED/register_lungs.m
@@ -1,11 +1,13 @@
 function [] = register_lungs(path_Data)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
+mkdir([path_Data '\Temp'])
+
 wt = myLoop(path_Data, ...
-    'elastix/',... % path to Elastix
+    'elastix\',... % path to Elastix
     {which('poc14.txt')});
 
-mkdir([path_Data '\Temp'])
+
 
 for i = 1 :3
     MED_t3(wt, i);
