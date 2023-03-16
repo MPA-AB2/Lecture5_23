9a724f8c85b06fdf943895fc33b24dd1b387a156
jakubmull <91261182+jakubmull@users.noreply.github.com>
2023-03-16 14:57:47 +0100

    Add files via upload

--- CSM/register_lungs.m
+++ CSM/register_lungs.m
@@ -1,52 +1,49 @@
-function register_lungs(path)
-cd(path)
-subfolders = dir;
-subfolders = subfolders(3:end,1); 
+function register_lungs(pathMain)
 
-for i = 3:length(subfolders)
-moving = niftiread([subfolders(i).name,'/moving.nii']);
+for i = 1:3
+path = [pathMain,'\pat0',num2str(i),'\'];
+
+moving = niftiread([path,'moving.nii']);
 
 mask = zeros(size(moving));
-mask(moving<9500) = 1;
-mask(moving>9500) = 0;
-niftiwrite(mask,"mask.nii")
-movefile ("mask.nii", subfolders(i).name)
+mask(moving<12000) = 1;
+mask(moving>12000) = 0;
 
+niftiwrite(mask,"mask.nii")
+movefile ("mask.nii", path)
 mkdir TempFile
 
-fixedImg = ([subfolders(i).name,'/fixed.nii']);
-movingImg = ([subfolders(i).name,'/moving.nii']);
-maskImg = ([subfolders(i).name,'/mask.nii']);
+fixedImg = ([path,'fixed.nii']);
+movingImg = ([path,'moving.nii']);
+maskImg = ([path,'mask.nii']);
 outputDirectory = ('TempFile');
-parameterFile = ('parameter_files/Parameters_Rigid.txt');
+addpath('parameter_files');
+paramPath= which('Parameters_Rigid.txt');
+parameterFile = (paramPath);
 
 cmd = append('elastix\elastix.exe', ' -f ', fixedImg, ' -m ', movingImg,' -mMask ', maskImg, ' -out ', outputDirectory, ' -p ', parameterFile);
 % run ELASTIX
 system(cmd);
 
-movefile ('TempFile/TransformParameters.0.txt',subfolders(i).name)
+movefile ('TempFile\TransformParameters.0.txt',path)
 
 outputDirectory = ("TempFile");
-parameterFile = ("parameter_files/Parameters_Rigid.txt");
-inputFile = ([subfolders(i).name,'/TransformParameters.0.txt']);
+
+
+% which
+inputFile = ([path,'TransformParameters.0.txt']);
 cmdT = append('elastix\transformix.exe', ' -def all -out ', outputDirectory, ' -tp ', inputFile);
 
 % run TRANSFORMIX
 system(cmdT);
 
-movefile('TempFile/deformationField.nii','TempFile/deformationField_Est.nii')
-movefile('TempFile/deformationField_Est.nii',subfolders(i).name)
+movefile('TempFile\deformationField.nii','TempFile\deformationField_Est.nii')
+movefile('TempFile\deformationField_Est.nii',path)
 
 % Delete Temp directory
 dirpath = 'TempFile';
 delete(fullfile(dirpath, '*'));
 rmdir TempFile
-
 end
-
-
-
-
-
 
 end
