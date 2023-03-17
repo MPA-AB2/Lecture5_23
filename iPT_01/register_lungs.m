cc7c6b97c1abfbf6e9c222942885454ae0c0174e
JakubicekRoman <jakubicek@vutbr.cz>
2023-03-17 15:07:00 +0100

    Signed-off-by: JakubicekRoman <jakubicek@vutbr.cz>

--- iPT_01/register_lungs.m
+++ iPT_01/register_lungs.m
@@ -5,10 +5,10 @@ function [] = register_lungs(path_Data)
     for rep = 1:3
         aktualnipat = ['pat0' num2str(rep)];
 
-        fixedpath = [cesta '\Data\' aktualnipat '\fixed.nii'];
-        movingpath = [cesta '\Data\' aktualnipat '\moving.nii'];
+        fixedpath = [cesta '\' aktualnipat '\fixed.nii'];
+        movingpath = [cesta '\' aktualnipat '\moving.nii'];
         vystup = [cesta '\TempFile'];
-        param = [cesta '\parameter_files\Param_me.txt'];
+        param = ['S:\UBMI\VYUKA\UCITEL\FABO2\2023\Lecture5\last\Lecture5\Lecture5_23\iPT_01' '\parameter_files\Param_me.txt'];
         
         transparm=[vystup '\TransformParameters.0.txt'];
 
@@ -16,17 +16,17 @@ function [] = register_lungs(path_Data)
         [~] = system(['elastix\transformix.exe',' -def ', ' all ',' -out ',vystup,' -tp ',transparm]);
         
         
-        movefile([vystup '\deformationField.nii'],[cesta '\Data\' aktualnipat '\deformationField_Est.nii']);
+        movefile([vystup '\deformationField.nii'],[cesta '\' aktualnipat '\deformationField_Est.nii']);
 
 
-        pom_vystup = [cesta '\Data\' aktualnipat]
+        pom_vystup = [cesta '\' aktualnipat]
         % load result nifti
         path_result = pom_vystup;
 
 
 
     end
-        path_Def_GT = [cesta '\Data']; %'\deformationField.nii'
+        path_Def_GT = [cesta '\']; %'\deformationField.nii'
         %path_Def_Est = [vystup '\deformationField_Est.nii'];
         [MSE,STD] = eval_lung([path_Def_GT])
 
